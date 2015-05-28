5448f26c7279734cc5f8c2379105b6f408f6af81
Joe Reiff <jcreiff@gmail.com>
2015-05-28 18:41:25 -0400

    create RoadTrip class and methods to call api

---
+++ app/models/road_trip.rb
@@ -0,0 +1,41 @@
+require 'httparty'
+
+class RoadTrip
+
+  attr_reader :start, :stop, :page
+
+  def initialize(start, stop)
+    @start = start
+    @stop = stop
+    @page = get_data
+  end
+
+  def get_data
+    HTTParty.get("https://maps.googleapis.com/maps/api/directions/json?origin=#{@start}&destination=#{@stop}&sensor=false&key=#{ENV["GOOGLE_KEY"]}")
+  end
+
+  def get_distance
+    @page["routes"][0]["legs"][0]["distance"]["text"]
+  end
+
+  def get_time
+    @page["routes"][0]["legs"][0]["duration"]["text"]
+  end
+
+  def start_location_lat
+    @page["routes"][0]["legs"][0]["start_location"]["lat"]
+  end
+
+  def start_location_long
+    @page["routes"][0]["legs"][0]["start_location"]["lng"]
+  end
+
+  def end_location_lat
+    @page["routes"][0]["legs"][0]["end_location"]["lat"]
+  end
+
+  def end_location_long
+    @page["routes"][0]["legs"][0]["end_location"]["lng"]
+  end
+
+end
