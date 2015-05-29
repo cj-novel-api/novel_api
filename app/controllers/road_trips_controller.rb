fa47711372124372e0191316809880a50da79328
Joe Reiff <jcreiff@gmail.com>
2015-05-28 23:28:05 -0400

    generated roadtrip controller

---
+++ app/controllers/road_trips_controller.rb
@@ -0,0 +1,15 @@
+class RoadTripsController < ApplicationController
+
+  def create
+
+  end
+
+  def index
+    #@roadtrips = RoadTrip.all(?)
+  end
+
+  def show
+    #@roadtrip = RoadTrip.find_by_id[params(:id)]
+  end
+
+end
