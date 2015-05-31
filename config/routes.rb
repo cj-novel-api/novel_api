cdfb981699ab1746130fc40e2a422bdb10ac520a
Cruz <nunez.a.cruz@gmail.com>
2015-05-31 17:51:32 -0400

    Refactor

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'gasprice/:fuel_type' => 'price#show'
   get 'trip/info' => 'trip#info'
   get 'roadtrip/info' => 'trip#info'
-  get 'summary/show' => 'summary#show'
+  get 'results' => 'summary#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
